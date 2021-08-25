478518550aaea54fe230bcb7984907c81c154b2c
hamzakhan100 <se.hamzakhan@gmail.com>
2021-08-25 20:16:14 +0500

    listing page design complet for web

--- src/features/listing-mangement/listing/listing.js
+++ src/features/listing-mangement/listing/listing.js
@@ -1,7 +1,17 @@
 import "./listing.css";
 import pic from "../newListing/assets/images/house.jpg";
-import { LocationOn, Home } from "@material-ui/icons";
+import {
+  LocationOn,
+  Home,
+  LocalParking,
+  Deck,
+  LocalFlorist,
+  OutdoorGrill,
+} from "@material-ui/icons";
+import VideocamIcon from "@material-ui/icons/Videocam";
 import { Button, TextField } from "@material-ui/core";
+import PoolIcon from "@material-ui/icons/Pool";
+
 import "date-fns";
 import React from "react";
 import Grid from "@material-ui/core/Grid";
@@ -23,7 +33,14 @@ const Listing = () => {
     hostName: "Sunni the stannis",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    amenities: { pool: true, garden: true, patio: true },
+    amenities: {
+      pool: true,
+      garden: true,
+      patio: true,
+      securityCam: true,
+      barbeque: true,
+      parking: true,
+    },
     rate: "44",
   };
   const [selectedDate, setSelectedDate] = React.useState(
@@ -115,11 +132,74 @@ const Listing = () => {
             </div>
             <div className="listingButton">
               <Button id="listingbutton" variant="contained">
-               Confirm Booking
+                Confirm Booking
               </Button>
             </div>
           </div>
         </div>
+      </div>
+      <div
+        ClassName="listingBottom"
+        style={{ padding: "10px 150px 40px 150px" }}
+      >
+        <div className="listingBottomLeft">
+          <div className="listingBottomLeft-layout">
+            <div className="listingBottomLeft-desc">
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum
+              </p>
+            </div>
+            <hr />
+            <div className="amenities">
+              <div className="amenities-heading">
+                <h3>what this listing offers</h3>
+              </div>
+              <div className="listingLeftAmenities">
+                <div className="d-flex">
+                  {" "}
+                  {obj.amenities.pool ? <PoolIcon /> : ""}{" "}
+                  <p className="p">Swimming Pool</p>
+                </div>
+                <div className="d-flex">
+                  {" "}
+                  {obj.amenities.garden ? <LocalFlorist /> : ""}{" "}
+                  <p className="p">Garden</p>
+                </div>
+                <div className="d-flex">
+                  {" "}
+                  {obj.amenities.patio ? <Deck /> : ""}{" "}
+                  <p className="p">Patio</p>
+                </div>
+                <div className="d-flex">
+                  {" "}
+                  {obj.amenities.securityCam ? <VideocamIcon /> : ""}{" "}
+                  <p className="p">Security camera</p>
+                </div>
+                <div className="d-flex">
+                  {" "}
+                  {obj.amenities.barbeque ? <OutdoorGrill /> : ""}{" "}
+                  <p className="p">Barbeque</p>
+                </div>
+                <div className="d-flex">
+                  {" "}
+                  {obj.amenities.parking ? <LocalParking /> : ""}{" "}
+                  <p className="p">Private Property</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="listingBottomRight"></div>
       </div>
     </div>
   );
